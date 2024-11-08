1efee3fe8bff0ed32903397bb6f0a334041ef8c9
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-08 15:43:53 +0200

    try 4

--- NumWords.java
+++ NumWords.java
@@ -7,7 +7,7 @@ public class NumWords {
 		int tens = ((num % 100) / 10);
 		int ones = ((num % 100) % 10);
 
-		System.out.println(hundreds + " hundreads, " + tens + " tens, " + ones + " ones.");
+		System.out.println(hundreds + " hundreds, " + tens + " tens, " + ones + " ones.");
 
 	}
 }
