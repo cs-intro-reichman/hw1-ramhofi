0d1fe91b0af84c8e9afb1e678e17ce0bfacb48f1
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-08 15:31:06 +0200

    try 3

--- NumWords.java
+++ NumWords.java
@@ -2,8 +2,12 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
-		int num = (Int)(% java NumWords 5109);
-		
+		int num = Integer.parseInt(args[0]);
+		int hundreds = (num / 100);
+		int tens = ((num % 100) / 10);
+		int ones = ((num % 100) % 10);
+
+		System.out.println(hundreds + " hundreads, " + tens + " tens, " + ones + " ones.");
 
 	}
 }
