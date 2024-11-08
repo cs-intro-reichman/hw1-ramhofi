98c5acd7fe026d191e2eb083f07fdb2f5eb149b6
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-08 19:44:05 +0200

    try7

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,22 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = ((int)((Math.random() * lim) + 1));
+		int b = ((int)((Math.random() * lim) + 1));
+		int c = ((int)((Math.random() * lim) + 1));
+
+		System.out.println (a + " " + b + " " + c );
+
+        
+		int d = Math.min(a , b);
+		int e = Math.min(d , c);
+		int f = Math.max(a , b);
+		int g = Math.max(c , f);
+		int h = (a + b + c - g -e);
+
+		System.out.println( e + " " + h + " " + g );
+
+
 	}
 }
