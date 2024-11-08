dc17ad4416364135d577b01274f8af2f7d0bdea0
ramhofi <ram.hofi@post.runi.ac.il>
2024-11-08 13:41:05 +0200

    try

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int money = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int year = Integer.parseInt(args[2]);
+
+		int futureValue = (int)(money * Math.pow(rate / 100 + 1, year));
+
+		System.out.println("After " + year + " years" + "," + " a" +  " $" + money + " saved" + " at " + rate + "%" + " will " + "yield " + futureValue);
+
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,8 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int num = (Int)(% java NumWords 5109);
+		
+
 	}
 }
